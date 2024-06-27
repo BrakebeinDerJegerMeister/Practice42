b7b25e84d3d842cda4b5ba668ce8ee61dd21272a
Prof Chaos <leperespikasse@gmail.com>
2024-06-27 11:26:10 +0200

    c00 ex05

--- Work/C/c00/ex05/ft_print_comb.c
+++ Work/C/c00/ex05/ft_print_comb.c
@@ -6,39 +6,45 @@
 /*   By: kaos <temp@temp.com>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/27 07:10:35 by kaos              #+#    #+#             */
-/*   Updated: 2024/06/27 07:28:33 by kaos             ###   ########.fr       */
+/*   Updated: 2024/06/27 11:25:21 by profchaos        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+
+void	print_combinaison(char i, char j, char k)
+{
+	char	s;
+	char	e;
+
+	s = ',';
+	e = ' ';
+	write(1, &i, 1);
+	write(1, &j, 1);
+	write(1, &k, 1);
+	if ((i != '7') || (j != '8') || (k != '9'))
+	{
+		write(1, &s, 1);
+		write(1, &e, 1);
+	}
+}
 
 void	ft_print_comb(void)
 {
 	char	i;
 	char	j;
 	char	k;
-	char	s;
-	char	e;
 
 	i = '0';
-	s = ',';
-	e = ' ';
 	while (i <= '7')
 	{
-		j = i+1;
+		j = i + 1;
 		while (j <= '8')
 		{
-			k = j+1;
+			k = j + 1;
 			while (k <= '9')
 			{
-				write(1, &i, 1);
-				write(1, &j, 1);
-				write(1, &k, 1);
-				if ((i != '7') & (j != '8') & (k != '9'))
-				{
-					write(1, &s, 1);
-					write(1, &e, 1);
-				}
+				print_combinaison(i, j, k);
 				k++;
 			}
 			j++;
@@ -47,8 +53,8 @@ void	ft_print_comb(void)
 	}
 }
 
-int	main(void)
+/*int	main(void)
 {
 	ft_print_comb();
-	return 0;
-}
+	return (0);
+}*/

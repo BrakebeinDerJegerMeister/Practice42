48df79a0ee3f967a08698da6bc76464228ec554d
Prof Chaos <leperespikasse@gmail.com>
2024-07-26 13:08:08 +0200

    c08 ex03

--- Work/C/c08/ex03/ft_point.h
+++ Work/C/c08/ex03/ft_point.h
@@ -6,17 +6,17 @@
 /*   By: profchaos <temp@temp.com>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/25 14:21:52 by profchaos         #+#    #+#             */
-/*   Updated: 2024/07/25 14:33:33 by profchaos        ###   ########.fr       */
+/*   Updated: 2024/07/26 09:52:14 by profchaos        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_POINT_H
 # define FT_POINT_H
 
-typedef struct	s_point
+typedef struct s_point
 {
 	int	x;
 	int	y;
-} t_point;
+}	t_point;
 
 #endif

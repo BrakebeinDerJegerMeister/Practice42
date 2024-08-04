021a15baad4da10881135db36f43df895cc4c5bd
Prof Chaos <leperespikasse@gmail.com>
2024-08-04 14:59:59 +0200

    c12 ex01

---
+++ Work/C/c12/ex01/ft_list_push_front.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_push_front.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kaos <temp@temp.com>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/04 14:31:00 by kaos              #+#    #+#             */
+/*   Updated: 2024/08/04 14:43:59 by kaos             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+
+void	ft_list_push_front(t_list **begin_list, void *data)
+{
+	t_list	*elem;
+
+	elem = ft_create_elem(data);
+	if (elem)
+	{
+		elem->data = data;
+		if (*begin_list)
+			elem->next = *begin_list;
+	}
+}

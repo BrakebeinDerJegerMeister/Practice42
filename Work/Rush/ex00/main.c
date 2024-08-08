fe1bab89a244b5ff0729849cd8686ff48bf17143
Prof Chaos <leperespikasse@gmail.com>
2024-08-08 16:45:53 +0200

    ...

--- Work/Rush/ex00/main.c
+++ Work/Rush/ex00/main.c
@@ -6,7 +6,7 @@
 /*   By: profchaos <temp@temp.com>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/08 14:20:50 by profchaos         #+#    #+#             */
-/*   Updated: 2024/08/08 15:30:09 by profchaos        ###   ########.fr       */
+/*   Updated: 2024/08/08 15:52:09 by profchaos        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,6 @@ void	rush(int x, int y);
 
 int	main(void)
 {
-	rush(1, 5);
+	rush(5, 1);
 	return (0);
 }
--- Work/Rush/ex00/rush01.c
+++ Work/Rush/ex00/rush01.c
@@ -6,8 +6,49 @@
 /*   By: profchaos <temp@temp.com>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/08 15:35:19 by profchaos         #+#    #+#             */
-/*   Updated: 2024/08/08 15:36:33 by profchaos        ###   ########.fr       */
+/*   Updated: 2024/08/08 15:54:39 by profchaos        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+void	ft_putchar(char c);
 
+void	print_corner(int cx, int cy, int x, int y)
+{
+	if (cx == 1 && cy == 1)
+		ft_putchar('/');
+	else if (cx == 1 && cy == y)
+		ft_putchar('\\');
+	else if (cx == x && cy == y)
+		ft_putchar('/');
+	else if (cx == x && cy == 1)
+		ft_putchar('\\');
+}
+
+void	print_other(int cx, int cy, int x, int y)
+{
+	if (((cx == 1 || cx == x) && (cy > 1 && cy < y))
+		|| ((cx > 1 && cx < x) && (cy == 1 || cy == y)))
+		ft_putchar('*');
+	else if (cx > 1 && cx < x && cy > 1 && cy < y)
+		ft_putchar(' ');
+}
+
+void	rush(int x, int y)
+{
+	int	cx;
+	int	cy;
+
+	cx = 0;
+	cy = 0;
+	while (++cy <= y)
+	{
+		while (++cx <= x)
+		{
+			print_corner(cx, cy, x, y);
+			print_other(cx, cy, x, y);
+			if (cx == x)
+				ft_putchar('\n');
+		}
+		cx = 0;
+	}
+}
